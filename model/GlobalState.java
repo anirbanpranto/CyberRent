6f4dde2eb6e1e08a535f57a7d8ac52cdf417031b
Anirban <anirbanpranto@gmail.com>
2021-10-23 07:02:31 +0800

    feat : Startup Global State

--- model/GlobalState.java
+++ model/GlobalState.java
@@ -10,10 +10,23 @@ import java.util.*;
 public class GlobalState {
     private Stage publicStage;
     private boolean isLoggedIn = false; //this becomes true when someone logs in
-    private Map<int, String> session; //this will store the email and the role
+    private Map<Integer, Owner> owenerLookup; //loaded when constructor is called
+    private Map<Integer, Agent> agentLookup; //loaded when constructor is called
+    private Map<Integer, Tenant> tenantLookup; //loaded when constructor is called
+    private Map<Integer, Admin> adminLookup; //loaded when constructor is called
+    private Map<Integer, Area> areaLookup; //load when constructor is called
+    /*
+      the point of having these lookup tables are related to the data that we're gonna display. For propeties, we need to show the data of affiliated Owners, Agents. This is also useful for logging in.
 
-    public GlobalState(){
+    */
 
+    private Map<int, String> session; //this will store the email and the role after login
+    private ArrayList<Property> propeties; //to be loaded in the globalstate constructor
+    private ArrayList<Property> personalProperty = null; //null at the begining, but its loaded when the login method is called
+    private ArrayList<Property> favoriteProperty = null; //null at the begining, but its loaded when the login method is called
+
+    private GlobalState(){
+        //load all properties here from database
     }
 
     public void saveStage(Stage s){
@@ -30,6 +43,18 @@ public class GlobalState {
 
     public Stage getStage(){
         return this.publicStage;
+    }
+
+    public ArrayList<Property> getProperties(){
+        return this.propeties;
+    }
+
+    public ArrayList<Property> getPersonalProperties(){
+        return this.personalProperty;
+    }
+
+    public ArrayList<Property> getFavoriteProperties(){
+        return this.favoriteProperty;
     }
 
     private static GlobalState instance;
