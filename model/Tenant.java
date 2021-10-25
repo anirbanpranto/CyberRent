d478dac39a46dd2806c762d49fe774273fb80ca8
Lee Jia Rou <leejiarou1029@gmail.com>
2021-10-25 23:55:06 +0800

    fix: Remove username and status

--- model/Tenant.java
+++ model/Tenant.java
@@ -5,13 +5,13 @@ public class Tenant extends User {
     private ArrayList<Property> favourite = new ArrayList<>(); // to store favourite property
     private static int idCount = Database.readUpdate("Tenant")+1; 
     public Tenant(int id, String fullname, String password, String email, String phoneNumber){
-        super(idCount,"username",fullname,password,email,"Active","Tenant",phoneNumber);
+        super(idCount,fullname,password,email,"Tenant",phoneNumber);
         
     }
 
     public static Tenant createTenant(String fullname, String password, String email, String phoneNumber){
         int id = ++idCount;
-        Database.writeData("Tenant", Arrays.asList(Integer.toString(idCount), fullname, "username", password, email, "Active", "Tenant", phoneNumber));
+        Database.writeData("Tenant", Arrays.asList(Integer.toString(idCount), fullname, password, email, "Tenant", phoneNumber));
         Database.writeUpdate("Tenant", Arrays.asList(Integer.toString(idCount)));
         
         return new Tenant(id, fullname, password, email, phoneNumber);
