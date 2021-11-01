3788262474e09d336349f23da84511bfd524ef95
Anirban Bala <anirbanpranto@gmail.com>
2021-11-01 15:28:20 +0800

    feat : view property controller

--- controller/view_propertyController.java
+++ controller/view_propertyController.java
@@ -7,16 +7,22 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 import model.GlobalState;
+import model.Property;
+import model.Agent;
+import model.Owner;
 
 import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
 
 public class view_propertyController {
 
-
+    private Property current_property;
 
     @FXML
     private Button currentPage;
@@ -47,6 +53,9 @@ public class view_propertyController {
 
     @FXML
     private Button profileButton;
+
+    @FXML
+    private Button switchToEditPost;
 
     @FXML
     private Label property1_Facilities_1;
@@ -120,6 +129,85 @@ public class view_propertyController {
     @FXML
     void addFavourite(ActionEvent event) {
         //no need
+    }
+
+    public void initialize(){
+        current_property = GlobalState.getInstance().getSelected();
+        property1_name.setText(current_property.getProjectName());
+        Label[] labels = {property1_Facilities_1, property1_Facilities_2, property1_Facilities_3};
+        for(int i = 0; i < current_property.getFacilities().size(); i++){
+            if(i < 3){
+                labels[i].setText(current_property.getFacilities().get(i));
+            }
+        }
+        Label[] labels2 = {property1_Features_1, property1_Features_2, property1_Features_3, property1_Features_4, property1_Features_5};
+        for(int i = 0; i < current_property.getKeyFeatures().size(); i++){
+            if(i < 5){
+                labels2[i].setText(current_property.getKeyFeatures().get(i));
+            }
+        }
+        if(current_property.getKeyFeatures().size() < 5){
+            for(int i = current_property.getKeyFeatures().size(); i < 5; i++){
+                labels2[i].setText(null);
+            }
+        }
+        if(current_property.getFacilities().size() < 3){
+            for(int i = current_property.getFacilities().size(); i < 3; i++){
+                labels[i].setText(null);
+            }
+        }
+        property1_bed.setText(Integer.toString(current_property.getNumberOfBedroom()));
+        property1_address.setText(current_property.getAddress());
+        property1_toilet.setText(Integer.toString(current_property.getNumberOfBathroom()));
+        property1_floorSize.setText(Integer.toString(current_property.getFloorSize()));
+        property1_PSF.setText(Double.toString(current_property.getpsf()));
+        property1_furnishStatus.setText(current_property.getFurnishStatus());
+        property1_type.setText(current_property.getPropertyType());
+        property1_price.setText(Integer.toString(current_property.getRental_price()));
+        try {
+            ImageView[] images = {property1_image_1, property1_image_2, property1_image_3};
+            for(int i = 0; i < current_property.getPhoto().size(); i++){
+                if(i < 3){
+                    String imgPath = current_property.getPhoto().get(i);
+                    imgPath = "../view/" + imgPath;
+                    Image img1;
+                    img1 = new Image(getClass().getResource(imgPath).toURI().toString());
+                    images[i].setImage(img1);
+                }
+            }
+        } catch (URISyntaxException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        int id = current_property.getListerID();
+        String role = current_property.getListerType();
+        if(role.equals("Agent")){
+            Agent agent = GlobalState.getInstance().getAgent(id);
+            if(agent==null){
+                lister_name.setText(null);
+                lister_phone.setText(null);
+                lister_agentLicense.setText(null);
+            }
+            else{
+                lister_name.setText(agent.getName());
+                lister_phone.setText(agent.getPhone());
+                lister_agentLicense.setText(agent.getLicenseNo());
+            }
+        }
+        if(role.equals("Owner")){
+            Owner owner = GlobalState.getInstance().getOwner(id);
+            if(owner==null){
+                lister_name.setText(null);
+                lister_phone.setText(null);
+                lister_agentLicense.setText(null);
+            }
+            else{
+                lister_name.setText(owner.getName());
+                lister_phone.setText(owner.getPhone());
+                lister_agentLicense.setText(null);
+            }
+        }
+        switchToEditPost.setVisible(GlobalState.getInstance().getIsPersonal());
     }
 
     @FXML
